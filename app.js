c5bfe7feadf108073fe91c07bd2978a91763191e
Irmak Tekin <irmak_tekin@epam.com>
2022-06-13 14:08:39 +0300

    jwt api

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const cors = require('cors');
+
+// initialize the app
+const app = express();
+//Middlewares
+app.use(bodyParser.urlencoded({
+extended:false
+}));
+
+app.use(bodyParser.json());
+app.use(cors());
+//setting the static directory
+app.use(express.static(path.join(__dirname, 'public')))
+
+//database configuration
+const db = require('./config/keys').mongoURI;
+mongoose.connect(db, {useNewUrlParser:true} ).then(() =>{
+    console.log(`Database connected successfully ${db}` )
+}).catch(err=>{
+    console.log(`Unable to connect with the database ${err}`)
+});
+const users= require('./routes/api/users');
+app.use('/api/users',users);
+
+app.get('/', (req,res) =>{
+    return res.send("<h1>Success</h1>")
+})
+
+
+const PORT=process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+    console.log(`Server started on port ${PORT}`);
+})

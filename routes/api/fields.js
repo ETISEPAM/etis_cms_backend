407b2ddc9ec194b820381afa273b60f6c7adf593
Emre Cetin <h.emrecetin@gmail.com>
2022-06-22 11:40:25 +0300

    Delete Rouıte Added to Fields

--- routes/api/fields.js
+++ routes/api/fields.js
@@ -49,4 +49,34 @@ router.post("/add", (req, res) => {
     });
 });
 
+router.get(":/id", (req, res) => {
+    Field.findById(req.params.id, (err, fields) => {
+        if (!err) {
+            res.status(200).json({ fields });
+        } else {
+            res.status(404).json({
+                success: false,
+                msg: `Field with Id: ${req.params.id} not found!`,
+            });
+        }
+    });
+});
+
+router.delete(":/id", (req, res) => {
+    const id = req.params.id;
+    Field.findByIdAndDelete(req.params.id, (err, fields) => {
+        if (err || !field) {
+            return res.status(404).json({
+                success: false,
+                msg: "The Field Not Found!",
+            });
+        } else {
+            return res.status(200).json({
+                success: true,
+                msg: "Field Deleted!",
+            });
+        }
+    });
+});
+
 module.exports = router;
